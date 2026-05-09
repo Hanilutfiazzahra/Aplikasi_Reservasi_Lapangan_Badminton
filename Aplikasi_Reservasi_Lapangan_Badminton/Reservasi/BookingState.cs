b8bb433d2a1e0c8b1c151997998a55be86681c92
rayen <rapriadi706@gmail.com>
2026-05-09 19:10:08 +0700

    Menambahkan fitur Reservasi

---
+++ Aplikasi_Reservasi_Lapangan_Badminton/Reservasi/BookingState.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Aplikasi_Reservasi_Lapangan_Badminton.Reservasi
+{
+    public enum BookingState
+    {
+        Pending,
+        Paid,
+        Completed,
+        Cancelled
+    }
+}
